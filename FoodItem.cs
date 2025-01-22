5b00a0e59f060cf72d7a39d7a2f609ff4edea09b
AndrewJP01 <andrewpotter300@gmail.com>
2025-01-22 13:59:16 -0700

    worked on code

---
+++ FoodItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3Assignment
+{
+    internal class FoodItem
+    {
+        // Declare the foodList as a static class-level variable
+        private static List<FoodItem> foodList = new List<FoodItem>();
+
+        // Properties to store the food details
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public string ExpirationDate { get; set; }
+
+        // Constructor to initialize the FoodItem object
+        public FoodItem(string fName, string fCategory, string fQuantity, string fExpDate)
+        {
+            Name = fName;
+            Category = fCategory;
+            // Directly parse and set Quantity (assuming it is always valid)
+            Quantity = int.Parse(fQuantity);
+            ExpirationDate = fExpDate;
+        }
+
+        public static void Food(string fName, string fCategory, string fQuantity, string fExpDate)
+        {
+            // Create a new FoodItem object using the constructor and add it to the list
+            FoodItem newFoodItem = new FoodItem(fName, fCategory, fQuantity, fExpDate);
+            foodList.Add(newFoodItem);
+
+        }
+
+        public void Food(string fName) 
+        { 
+            foodList.Remove
+
+        }
+
+        public void Food()
+        {
+
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mission3Assignment;
+
+
+        while (true)
+        {
+            Console.WriteLine("Welcome to the Food Bank Program!");
+            Console.WriteLine("1. Add Food Items");
+            Console.WriteLine("2. Delete Food Items");
+            Console.WriteLine("3. Print List of Current Food Items");
+            Console.WriteLine("4. Exit the Program");
+            Console.Write("Type in the number of what you'd like to do: ");
+            string userInput = Console.ReadLine();
+
+            if (int.TryParse(userInput, out int choice) && (choice >= 1 && choice <= 4))
+            {
+                Console.WriteLine($"You selected option {choice}.");
+
+                // Perform action based on the valid choice
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter the Food Name: ");
+                        string fName = Console.ReadLine();
+
+                        Console.Write("Enter the Food Category: ");
+                        string fCategory = Console.ReadLine();
+
+                        Console.Write("Enter the Food Quantity: ");
+                        string fQuantity = "";
+
+                        while (true)
+                        {
+                            fQuantity = Console.ReadLine();
+                            if (int.TryParse(fQuantity, out int parsedQuantity) && parsedQuantity >= 0)
+                            {
+                                // Valid input, proceed with parsedQuantity
+                                break; // Exit the loop when the input is valid
+                            }
+                            Console.WriteLine("Invalid quantity. Please enter a non-negative integer.");
+                            Console.Write("Enter the Food Quantity: ");
+                        }
+
+                        Console.Write("Enter the Food Expiration Date: ");
+                        string fExpDate = Console.ReadLine();
+                        FoodItem.Food(fName, fCategory, fQuantity, fExpDate);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Deleting food items...");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Printing list of current food items...");
+                        break;
+                        
+                    case 4:
+                        Console.WriteLine("Exiting the program. Goodbye!");
+                        return; // Exit the program
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please enter a number between 1 and 4.\n");
+            }
+        }
+
+
+
+
+
+
+
